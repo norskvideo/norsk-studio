ea681225371011630e01a070a375ec23d58560c7
Rob Ashton <robashton@codeofrob.com>
2024-07-08 13:19:02 +0100

    ensure the rtmp output doesn't mind inputs changing

--- workspaces/built-ins/src/test/rtmp-output.ts
+++ workspaces/built-ins/src/test/rtmp-output.ts
@@ -6,9 +6,9 @@ import * as document from '@norskvideo/norsk-studio/lib/runtime/document';
 import YAML from 'yaml';
 import go from '@norskvideo/norsk-studio/lib/runtime/execution';
 import { RtmpOutputEvent, RtmpOutputSettings, RtmpOutputState } from "../output.rtmp/runtime";
-import { testSourceDescription, videoAndAudio } from "@norskvideo/norsk-studio/lib/test/_util/sources";
+import { testSourceDescription, video, videoAndAudio } from "@norskvideo/norsk-studio/lib/test/_util/sources";
 import RtmpInfo from "../output.rtmp/info";
-import { Av, RegistrationConsts } from "@norskvideo/norsk-studio/lib/extension/client-types";
+import { Av, RegistrationConsts, Video } from "@norskvideo/norsk-studio/lib/extension/client-types";
 import { SimpleSinkWrapper } from "@norskvideo/norsk-studio/lib/extension/base-nodes";
 import { StudioNodeSubscriptionSource } from "@norskvideo/norsk-studio/lib/extension/runtime-types";
 import { FfprobeStream, getStreams } from "@norskvideo/norsk-studio/lib/test/_util/sinks";
@@ -42,11 +42,11 @@ describe("RTMP Output", () => {
   let norsk: Norsk | undefined = undefined;
   let ffprobe: Promise<FfprobeStream[]> = undefined!;
 
-  after(async () => {
+  afterEach(async () => {
     await norsk?.close();
   })
 
-  before(async () => {
+  it("Provides an RTMP output when there is a stream", async () => {
     ffprobe = getStreams('rtmp://127.0.0.1:5001/norsk/output');
     norsk = await Norsk.connect({ onShutdown: () => { } });
     const compiled = await testDocument();
@@ -58,11 +58,35 @@ describe("RTMP Output", () => {
       testSourceDescription(),
       { type: "take-first-stream", select: Av }
     )], { requireOneOfEverything: true })
-  })
-
-  it("Should be running an RTMP output", async () => {
     const streams = await ffprobe;
     expect(streams ?? []).lengthOf(2);
+  })
+
+  it("Provides an RTMP output when the stream changes", async () => {
+    norsk = await Norsk.connect({ onShutdown: () => { } });
+    const compiled = await testDocument();
+    const result = await go(norsk, compiled);
+    const source = await video(norsk, 'source1');
+    const rtmp = result.components["rtmp"] as SimpleSinkWrapper;
+    rtmp.subscribe([new StudioNodeSubscriptionSource(
+      source,
+      testSourceDescription(),
+      { type: "take-first-stream", select: Video }
+    )], { requireOneOfEverything: true })
+
+    ffprobe = getStreams('rtmp://127.0.0.1:5001/norsk/output');
+    const streams1 = await ffprobe;
+    expect(streams1 ?? []).lengthOf(1);
+    const source2 = await videoAndAudio(norsk, 'source2');
+    rtmp.subscribe([new StudioNodeSubscriptionSource(
+      source2,
+      testSourceDescription(),
+      { type: "take-first-stream", select: Av }
+    )], { requireOneOfEverything: true })
+    ffprobe = getStreams('rtmp://127.0.0.1:5001/norsk/output');
+    const streams2 = await ffprobe;
+    expect(streams2 ?? []).lengthOf(2);
+
   })
 });
 
