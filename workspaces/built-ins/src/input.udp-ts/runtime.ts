a4bc9d22ce2a18c6a9b8c0505cf8bd0ce214df13
Rob Ashton <robashton@codeofrob.com>
2024-07-10 08:42:59 +0100

    de-bodge udp input alignment

--- workspaces/built-ins/src/input.udp-ts/runtime.ts
+++ workspaces/built-ins/src/input.udp-ts/runtime.ts
@@ -1,4 +1,4 @@
-import { Norsk, UdpTsInputSettings as SdkSettings, requireAV, selectAll } from '@norskvideo/norsk-sdk';
+import { Norsk, UdpTsInputSettings as SdkSettings } from '@norskvideo/norsk-sdk';
 import { SimpleInputWrapper } from '@norskvideo/norsk-studio/lib/extension/base-nodes';
 import { OnCreated, ServerComponentDefinition } from '@norskvideo/norsk-studio/lib/extension/runtime-types';
 export type UdpTsInputSettings = Pick<SdkSettings, 'interface' | 'timeout' | 'rtpDecapsulate' | 'sourceName' | 'ip' | 'port'> & {
@@ -8,22 +8,9 @@ export type UdpTsInputSettings = Pick<SdkSettings, 'interface' | 'timeout' | 'rt
 
 export default class UdpTsInputDefinition implements ServerComponentDefinition<UdpTsInputSettings, SimpleInputWrapper> {
   async create(norsk: Norsk, cfg: UdpTsInputSettings, cb: OnCreated<SimpleInputWrapper>) {
-    const wrapper = new SimpleInputWrapper(cfg.id, async () => {
-
-      const udp = await norsk.input.udpTs({ ...cfg });
-
-      // TODO this is bodged in for NAB AWS purposes
-      const align = await norsk.processor.transform.streamAlign({
-        frameRate: { frames: 25, seconds: 1 },
-        sampleRate: 96000,
-        syncAv: true
-      })
-      align.subscribe([{
-        source: udp, sourceSelector: selectAll
-      }], requireAV);
-
-      return align;
-    })
+    const wrapper = new SimpleInputWrapper(cfg.id, async () =>
+      norsk.input.udpTs({ ...cfg })
+    )
     await wrapper.initialised;
     cb(wrapper);
   }
