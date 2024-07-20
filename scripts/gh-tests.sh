ab67900b5ac8832d4b0cb4dcb3a890ff3fb0c169
robashton <robashton@codeofrob.com>
2024-07-20 18:08:40 +0100

    truy to get better formatting herte

--- scripts/gh-tests.sh
+++ scripts/gh-tests.sh
@@ -5,11 +5,11 @@ cat $PWD/built-ins.json $PWD/vd.json | jq -s '{
   "content": "Norsk Studio Defaults - Tests",
   "embeds": [ { "title": (if .[0].stats.failures == 0 then "Built-ins: Success" else "Built-ins: Failure" end)
               , "color": (if .[0].stats.failures == 0 then 5763719 else 15548997 end)
-              , "description": (if .[0].stats.failures == 0 then "All tests passed" else [.[0].failures.[].title] | join("\r\n - ")) 
+              , "description": (if .[0].stats.failures == 0 then "All tests passed" else ("Failures \r\n===\r\n" + ([.[0].failures.[].title] | join("\r\n - "))) end) 
               },
               { "title": (if .[1].stats.failures == 0 then "Vision Director: Success" else "Vision Director: Failure" end)
               , "color": (if .[1].stats.failures == 0 then 5763719 else 15548997 end)
-              , "description": (if .[1].stats.failures == 0 then "All tests passed" else [.[1].failures.[].title] | join("\r\n - ")) 
+              , "description": (if .[1].stats.failures == 0 then "All tests passed" else ("Failures \r\n===\r\n" + ([.[1].failures.[].title] | join("\r\n - "))) end) 
               }            
            ]
     }' > discord.json
