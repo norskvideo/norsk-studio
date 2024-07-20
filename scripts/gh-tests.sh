32cbad082c1dfd9a067237381f45914f9c6ca98e
robashton <robashton@codeofrob.com>
2024-07-20 17:18:17 +0100

    build reports for tests

---
+++ scripts/gh-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+LOG_LEVEL=error npm run test --workspace workspaces/built-ins -- --reporter mocha-json-output-reporter --reporter-options output=$PWD/built-ins.json
+LOG_LEVEL=error npm run test --workspace workspaces/vision-director -- --reporter mocha-json-output-reporter --reporter-options output=$PWD/vd.json
+cat $PWD/built-ins.json $PWD/vd.json | jq -s '{ 
+  "content": "Norsk Studio Defaults - Tests",
+  "embeds": [ { "title": (if .[0].stats.failures == 0 then "Built-ins: Success" else "Built-ins: Failure" end)
+              , "color": (if .[0].stats.failures == 0 then 5763719 else 15548997 end)
+              , "description": (if .[0].stats.failures == 0 then "All tests passed" else .[0].failures.title end) 
+              },
+              { "title": (if .[1].stats.failures == 0 then "Vision Director: Success" else "Vision Director: Failure" end)
+              , "color": (if .[1].stats.failures == 0 then 5763719 else 15548997 end)
+              , "description": (if .[1].stats.failures == 0 then "All tests passed" else .[1].failures.title end) 
+              }            
+           ]
+    }' > discord.json
+cat discord.json
