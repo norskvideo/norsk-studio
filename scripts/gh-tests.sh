5c6448e8f5d3b80daf81ebc73eae80247b971eb0
robashton <robashton@codeofrob.com>
2024-07-21 12:10:04 +0100

    update deps

--- scripts/gh-tests.sh
+++ scripts/gh-tests.sh
@@ -7,11 +7,11 @@ cat $PWD/built-ins.json $PWD/vd.json | jq -s '{
   "username": "Evil Jenkins",
   "embeds": [ { "title": (if .[0].stats.failures == 0 then "Built-ins: Success" else "Built-ins: Failure" end)
               , "color": (if .[0].stats.failures == 0 then 5763719 else 15548997 end)
-              , "description": (if .[0].stats.failures == 0 then "All tests passed" else ("Failures \r\n===\r\n- " + ([.[0].failures.[].title] | join("\r\n- "))) end) 
+              , "description": (if .[0].stats.failures == 0 then ((.[0].stats.passes | tostring) + " tests passed") else ("Failures \r\n===\r\n- " + ([.[0].failures.[].title] | join("\r\n- "))) end) 
               },
               { "title": (if .[1].stats.failures == 0 then "Vision Director: Success" else "Vision Director: Failure" end)
               , "color": (if .[1].stats.failures == 0 then 5763719 else 15548997 end)
-              , "description": (if .[1].stats.failures == 0 then "All tests passed" else ("Failures \r\n===\r\n- " + ([.[1].failures.[].title] | join("\r\n- "))) end) 
+              , "description": (if .[1].stats.failures == 0 then ((.[1].stats.passes | tostring) + " tests passed") else ("Failures \r\n===\r\n- " + ([.[1].failures.[].title] | join("\r\n- "))) end) 
               }            
            ]
     }' > discord.json
