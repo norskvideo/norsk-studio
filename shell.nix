85bfa791ba32c571c89ecd96f1b852ee2019d524
kelvinkirima014 <kelvinkirima2@gmail.com>
2024-08-16 09:43:03 +0300

    use git tarball in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,32 +1,12 @@
 # { pkgs ? import <nixpkgs> {} }:
 let
 
-  # try to keep this in line with Norsk
-  pinnedNixHash = "fa9a51752f1b5de583ad5213eb621be071806663";
-  pinnedNix =
-    builtins.fetchGit {
-      name = "nixpkgs-pinned";
-      url = "https://github.com/NixOS/nixpkgs.git";
-      rev = "${pinnedNixHash}";
-    };
+  pinnedNixHash = "57610d2f8f0937f39dbd72251e9614b1561942d8"; # This hash is from 2024-06-01
+  pinnedNix = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/${pinnedNixHash}.tar.gz";
 
   nixpkgs = import pinnedNix {};
 
   ffmpegForTests = nixpkgs.ffmpeg-full;
-
-  # ffmpegForTests = (nixpkgs.ffmpeg-full.override { 
-  # withGme = false; 
-  # withVmaf = true;
-
-  # # https://github.com/NixOS/nixpkgs/commit/0f0b89fc7bcea595d006f8323f40bb75c8a230af#diff-553d3a02dcec4459499ae8606548394f86a283651d828b2661b232f0c0aed5caR31
-  # x264 = nixpkgs.x264.overrideAttrs (old: {
-  #   postPatch = old.postPatch
-  #     + nixpkgs.lib.optionalString (nixpkgs.stdenv.isDarwin) ''
-  #       substituteInPlace Makefile --replace '$(if $(STRIP), $(STRIP) -x $@)' '$(if $(STRIP), $(STRIP) -S $@)'
-  #     '';
-  # });
-  
-  #  });
 
   chromium = (nixpkgs.chromium); # .override { channel = "dev"; });
 
