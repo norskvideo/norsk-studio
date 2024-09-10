a769fc8a704b9beca4fdab7566e60bbc4f44a0c4
Adrian Roe <roe.adrian@gmail.com>
2024-09-10 18:56:16 +0100

    More OpenAPI games

--- workspaces/built-ins/src/processor.onscreenGraphic/types.ts
+++ workspaces/built-ins/src/processor.onscreenGraphic/types.ts
@@ -162,6 +162,101 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/active-graphic": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /**
+         * Info about the current graphic
+         * @description Returns the configuration for current the onscreen graphic is any
+         */
+        get: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description Details of the configured graphic */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "application/json": components["schemas"]["config"];
+                    };
+                };
+                /** @description No graphic configured */
+                204: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content?: never;
+                };
+                /** @description Component not found */
+                404: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content?: never;
+                };
+            };
+        };
+        put?: never;
+        /**
+         * Change the current graphic file and/or position
+         * @description Change the current graphic file and/or position
+         */
+        post: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            /** @description The graphic filename and location (sending an empty JSON object will delete the graphic) */
+            requestBody?: {
+                content: {
+                    "application/json": components["schemas"]["config"] | components["schemas"]["emptyObject"];
+                };
+            };
+            responses: {
+                /** @description The active graphic was successfully updated */
+                204: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content?: never;
+                };
+                /** @description Invalid request */
+                400: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "application/json": components["schemas"]["errorResponse"];
+                    };
+                };
+                /** @description Component not found */
+                404: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content?: never;
+                };
+            };
+        };
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -181,6 +276,15 @@ export interface components {
         config: {
             graphic?: components["schemas"]["graphic"];
             position?: components["schemas"]["position"];
+        };
+        /** @example {} */
+        emptyObject: Record<string, never>;
+        /** @description Details of why the request failed */
+        errorResponse: {
+            /** @description A description of the error */
+            error?: string;
+            /** @description The unacceptable data */
+            details?: string;
         };
     };
     responses: never;
