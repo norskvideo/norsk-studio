db4adfecb7e1d13e32eb11905e5f4b8fe9008227
kelvinkirima014 <kelvinkirima2@gmail.com>
2024-09-11 18:42:18 +0300

    Make srt options optional

--- workspaces/built-ins/src/shared/srt-socket-options.ts
+++ workspaces/built-ins/src/shared/srt-socket-options.ts
@@ -5,31 +5,31 @@ const result = ({ Z }: Validation): ConfigForm<SocketOptions> => ({
   receiveLatency: {
     help: "The latency value in the receiving direction of the socket (SRTO_RCVLATENCY)",
     hint: {
-      type: "numeric", validation: Z.optional(Z.number())
+      type: "numeric", optional: true, validation: Z.optional(Z.number())
     },
   },
   peerLatency: {
     help: "The latency value provided by the sender side as a minimum value for the receiver (SRTO_PEERLATENCY)",
     hint: {
-      type: "numeric", validation: Z.optional(Z.number())
+      type: "numeric", optional: true, validation: Z.optional(Z.number())
     }
   },
   inputBandwidth: {
-    help: "Input bandwidth (SRTO_INPUTBW xxx",
+    help: "Input bandwidth (SRTO_INPUTBW)",
     hint: {
-      type: "numeric", validation: Z.optional(Z.number())
+      type: "numeric", optional: true, validation: Z.optional(Z.number())
     }
   },
   overheadBandwidth: {
     help: "Overhead bandwidth (SRTO_OHEADBW)",
     hint: {
-      type: "numeric", validation: Z.optional(Z.number())
+      type: "numeric", optional: true, validation: Z.optional(Z.number())
     }
   },
   maxBandwidth: {
     help: "Max bandwidth (SRTO_MAXBW)",
     hint: {
-      type: "numeric", validation: Z.optional(Z.number())
+      type: "numeric", optional: true, validation: Z.optional(Z.number())
     }
   },
 }
