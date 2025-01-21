cf27aa0a62b23d2a81f31fc068aac8e898a7adee
Verity Scheel <monoidmusician@gmail.com>
2025-01-20 22:38:11 -0600

    Tailwind config tweaks now handled upstream

--- workspaces/source-switcher/tailwind.config.js
+++ workspaces/source-switcher/tailwind.config.js
@@ -3,12 +3,4 @@ const defaultTheme = require('@norskvideo/norsk-studio/tailwind-theme')
 module.exports = {
   ...defaultTheme,
   content: ["src/**/*.{html,js,ejs,ts,tsx}"],
-  corePlugins: {
-    // Preflight is already included by the core studio stylesheet
-    preflight: false,
-  },
-  experimental: {
-    // This removes the var(--tw-*) reset
-    optimizeUniversalDefaults: true,
-  },
 }
