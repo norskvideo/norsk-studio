6201c726a33fa4f344529b363a1e0149dd55b64d
Rob Ashton <robashton@codeofrob.com>
2025-02-11 20:46:30 +0000

    yeet crypto out of hls/ts

--- workspaces/built-ins/src/output.autoHls/info.ts
+++ workspaces/built-ins/src/output.autoHls/info.ts
@@ -3,11 +3,18 @@ import AutoCmaf from '../output.autoCmaf/info';
 
 export default function(R: Registration): ReturnType<typeof AutoCmaf> {
   const autoCmaf = AutoCmaf(R);
+
+  const { drmProvider: _, ...NoCrypto } = autoCmaf.configForm.form;
+
   return {
     ...autoCmaf,
     identifier: 'output.autoHls',
     name: "Auto HLS(TS)",
     description: "This component handles the creation of HLS/TS outputs from multiple video and audio streams.",
+    configForm: {
+      ...autoCmaf.configForm,
+      form: { ...NoCrypto }
+    }
   }
 }
 
