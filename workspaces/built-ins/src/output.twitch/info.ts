b38f307caf4bb28d78bd02aab3509afcc8897456
kelvinkirima014 <kelvinkirima2@gmail.com>
2025-02-24 15:08:57 +0300

    Add twitch output component

---
+++ workspaces/built-ins/src/output.twitch/info.ts
@@ -0,0 +1,43 @@
+import type { TwitchOutputSettings } from "./runtime";
+import type Registration from "@norskvideo/norsk-studio/lib/extension/registration";
+
+export default function ({
+  defineComponent,
+  All,
+  validation: { Z },
+}: Registration) {
+  return defineComponent<TwitchOutputSettings>({
+    identifier: 'output.twitch',
+    category: 'output',
+    name: "Twitch Live",
+    description: "Stream directly to Twitch using RTMP",
+    subscription: {
+      accepts: {
+        type: 'multi-stream',
+        media: All
+      },
+    },
+    display: (_desc) => {
+      return {
+      }
+    },
+    configForm: {
+      form: {
+        streamKey: {
+          help: "Twitch Stream Key (from Twitch Creator Dashboard)",
+          hint: {
+            type: 'text',
+            validation: Z.string().min(1),
+          }
+        },
+        notes: {
+          help: "Additional notes about this component",
+          hint: {
+            type: 'text',
+            optional: true
+          }
+        },
+      }
+    }
+  });
+}
