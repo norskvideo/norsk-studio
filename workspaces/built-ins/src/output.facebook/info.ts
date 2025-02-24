909d992960eecac278452a291b92f182fd8aa2df
kelvinkirima014 <kelvinkirima2@gmail.com>
2025-02-24 15:08:15 +0300

    Add facebook output component

---
+++ workspaces/built-ins/src/output.facebook/info.ts
@@ -0,0 +1,43 @@
+import type { FacebookOutputSettings } from "./runtime";
+import type Registration from "@norskvideo/norsk-studio/lib/extension/registration";
+
+export default function({
+  defineComponent,
+  All,
+  validation: { Z },
+}: Registration) {
+  return defineComponent<FacebookOutputSettings>({
+    identifier: 'output.Facebook',
+    category: 'output',
+    name: "Facebook Live",
+    description: "Stream directly to Facebook Live using RTMP",
+    subscription: {
+      accepts: {
+        type: 'multi-stream',
+        media: All
+      },
+    },
+    display: () => {
+      return {
+      }
+    },
+    configForm: {
+      form: {
+        streamKey: { 
+          help: "Facebook Stream Key", 
+          hint: { 
+            type: 'text', 
+            validation: Z.string().min(1),
+          }
+        },
+        notes: { 
+          help: "Additional notes about this component", 
+          hint: { 
+            type: 'text', 
+            optional: true 
+          } 
+        },
+      }
+    }
+  });
+}
