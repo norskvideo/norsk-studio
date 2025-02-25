5d1390f50876829dcb6d04bb4443bebeabbb3332
Rob Ashton <robashton@codeofrob.com>
2025-02-25 08:56:20 +0000

    prep for base class

--- workspaces/built-ins/src/output.rtmp/runtime.ts
+++ workspaces/built-ins/src/output.rtmp/runtime.ts
@@ -1,7 +1,7 @@
 import { Norsk, ReceiveFromAddressAuto, RtmpConnectionFailureReason, RtmpOutputNode, RtmpOutputSettings as SdkSettings } from '@norskvideo/norsk-sdk';
 
-import { CreatedMediaNode, InstanceRouteInfo, OnCreated, ServerComponentDefinition, StudioNodeSubscriptionSource, StudioRuntime } from '@norskvideo/norsk-studio/lib/extension/runtime-types';
-import { CustomSinkNode, SubscriptionOpts } from '@norskvideo/norsk-studio/lib/extension/base-nodes';
+import { CreatedMediaNode, InstanceRouteInfo, OnCreated, RelatedMediaNodes, ServerComponentDefinition, StudioNodeSubscriptionSource, StudioRuntime } from '@norskvideo/norsk-studio/lib/extension/runtime-types';
+import { SubscriptionOpts } from '@norskvideo/norsk-studio/lib/extension/base-nodes';
 import { ContextPromiseControl } from '@norskvideo/norsk-studio/lib/runtime/util';
 import { debuglog } from '@norskvideo/norsk-studio/lib/server/logging';
 import { resolveRefs } from 'json-refs';
@@ -38,116 +38,6 @@ export type RtmpOutputEvent = {
 export type RtmpOutputCommand = {
   type: "enable-output" | "disable-output"
 }
-
-class RtmpOutput extends CustomSinkNode {
-  initialised: Promise<void>;
-  norsk: Norsk;
-  runtime: StudioRuntime<RtmpOutputState, RtmpOutputCommand, RtmpOutputEvent>;
-
-  cfg: RtmpOutputSettings;
-  rtmp?: RtmpOutputNode;
-  enabled: boolean = true;
-  control: ContextPromiseControl = new ContextPromiseControl(this.subscribeImpl.bind(this));
-
-  currentSources: Map<CreatedMediaNode, StudioNodeSubscriptionSource> = new Map();
-
-  constructor(norsk: Norsk, runtime: StudioRuntime<RtmpOutputState, RtmpOutputCommand, RtmpOutputEvent>, cfg: RtmpOutputSettings) {
-    super(cfg.id);
-    this.cfg = cfg;
-    this.norsk = norsk;
-    this.runtime = runtime;
-    this.initialised = Promise.resolve();
-  }
-
-  async enableOutput() {
-    if (!this.enabled) {
-      this.enabled = true;
-      await this.control.schedule();
-      this.runtime.updates.raiseEvent({ type: 'output-enabled' });
-      debuglog("Output enabled", { id: this.id });
-    }
-  }
-
-  async disableOutput() {
-    if (this.enabled) {
-      this.enabled = false;
-      if (this.rtmp) {
-        await this.rtmp.close();
-        this.rtmp = undefined;
-      }
-      this.runtime.updates.raiseEvent({
-        type: 'output-disabled'
-      })
-      debuglog("Output disabled", { id: this.id });
-    }
-  }
-
-  override subscribe(sources: StudioNodeSubscriptionSource[], _opts?: SubscriptionOpts): void {
-    this.currentSources = new Map();
-    sources.forEach((s) => {
-      this.currentSources.set(s.source, s);
-    });
-
-    this.control.setSources(sources);
-  }
-
-  async subscribeImpl(sources: StudioNodeSubscriptionSource[]) {
-    if (!this.enabled) {
-      debuglog("Skipping context handling - output disabled", { id: this.id });
-      return;
-    }
-
-    //for (const [_, source] of this.currentSources) {
-    // if (source) {
-    const videoSource = sources.filter((s) => s.streams.select.includes("video")).at(0)?.selectVideo();
-    const audioSource = sources.filter((s) => s.streams.select.includes("audio")).at(0)?.selectAudio();
-
-    const subscriptions: ReceiveFromAddressAuto[] = [];
-
-    if (videoSource && videoSource.length > 0) {
-      subscriptions.push(videoSource[0]);
-    }
-
-    if (audioSource && audioSource.length > 0) {
-      subscriptions.push(audioSource[0]);
-    }
-
-    if (subscriptions.length > 0) {
-      if (!this.rtmp) {
-        const rtmpCfg: SdkSettings = {
-          id: this.cfg.id,
-          url: this.cfg.url,
-          bufferDelayMs: this.cfg.bufferDelayMs,
-          avDelayMs: this.cfg.avDelayMs,
-          retryConnectionTimeout: this.cfg.retryConnectionTimeout,
-          onPublishStart: () => {
-            this.runtime.updates.clearAlert('connection-error');
-            this.runtime.updates.raiseEvent({ type: "rtmp-server-connected-and-publishing" })
-          },
-          onConnectionFailure: (reason: RtmpConnectionFailureReason) => {
-            this.runtime.updates.setAlert('connection-error', { level: 'error', message: "Failed to connect" });
-            switch (reason) {
-              case RtmpConnectionFailureReason.RtmpConnectionFailedRetry:
-                this.runtime.updates.raiseEvent({ type: "rtmp-server-connection-failed-retry" })
-                break;
-              default:
-                assertUnreachable(reason)
-            }
-          }
-        }
-        this.rtmp = await this.norsk.output.rtmp(rtmpCfg);
-        this.setup({ sink: this.rtmp, updates: this.runtime.updates }, { requireOneOfEverything: true });
-      }
-      this.rtmp.subscribe(subscriptions, (ctx) => {
-        return ctx.streams.length === subscriptions.length;
-      })
-    } else {
-      await this.rtmp?.close();
-      this.rtmp = undefined;
-    }
-  }
-}
-
 
 export default class RtmpOutputDefinition implements ServerComponentDefinition<RtmpOutputSettings, RtmpOutput, RtmpOutputState, RtmpOutputCommand, RtmpOutputEvent> {
   async create(norsk: Norsk, cfg: RtmpOutputSettings, cb: OnCreated<RtmpOutput>, runtime: StudioRuntime<RtmpOutputState, RtmpOutputCommand, RtmpOutputEvent>) {
@@ -217,3 +107,113 @@ export default class RtmpOutputDefinition implements ServerComponentDefinition<R
 function assertUnreachable(_: never): never {
   throw new Error("Didn't expect to get here");
 }
+
+class RtmpOutput implements CreatedMediaNode {
+  initialised: Promise<void>;
+  norsk: Norsk;
+  runtime: StudioRuntime<RtmpOutputState, RtmpOutputCommand, RtmpOutputEvent>;
+
+  cfg: RtmpOutputSettings;
+  rtmp?: RtmpOutputNode;
+  enabled: boolean = true;
+  control: ContextPromiseControl = new ContextPromiseControl(this.subscribeImpl.bind(this));
+  relatedMediaNodes: RelatedMediaNodes = new RelatedMediaNodes();
+  id: string;
+
+  currentSources: Map<CreatedMediaNode, StudioNodeSubscriptionSource> = new Map();
+
+  constructor(norsk: Norsk, runtime: StudioRuntime<RtmpOutputState, RtmpOutputCommand, RtmpOutputEvent>, cfg: RtmpOutputSettings) {
+    this.id = cfg.id;
+    this.cfg = cfg;
+    this.norsk = norsk;
+    this.runtime = runtime;
+    this.initialised = Promise.resolve();
+  }
+
+  async enableOutput() {
+    if (!this.enabled) {
+      this.enabled = true;
+      await this.control.schedule();
+      this.runtime.updates.raiseEvent({ type: 'output-enabled' });
+      debuglog("Output enabled", { id: this.id });
+    }
+  }
+
+  async disableOutput() {
+    if (this.enabled) {
+      this.enabled = false;
+      if (this.rtmp) {
+        await this.rtmp.close();
+        this.rtmp = undefined;
+      }
+      this.runtime.updates.raiseEvent({
+        type: 'output-disabled'
+      })
+      debuglog("Output disabled", { id: this.id });
+    }
+  }
+
+  subscribe(sources: StudioNodeSubscriptionSource[], _opts?: SubscriptionOpts): void {
+    this.currentSources = new Map();
+    sources.forEach((s) => {
+      this.currentSources.set(s.source, s);
+    });
+
+    this.control.setSources(sources);
+  }
+
+  async subscribeImpl(sources: StudioNodeSubscriptionSource[]) {
+    if (!this.enabled) {
+      debuglog("Skipping context handling - output disabled", { id: this.id });
+      return;
+    }
+
+    const videoSource = sources.filter((s) => s.streams.select.includes("video")).at(0)?.selectVideo();
+    const audioSource = sources.filter((s) => s.streams.select.includes("audio")).at(0)?.selectAudio();
+
+    const subscriptions: ReceiveFromAddressAuto[] = [];
+
+    if (videoSource && videoSource.length > 0) {
+      subscriptions.push(videoSource[0]);
+    }
+
+    if (audioSource && audioSource.length > 0) {
+      subscriptions.push(audioSource[0]);
+    }
+
+    if (subscriptions.length > 0) {
+      if (!this.rtmp) {
+        const rtmpCfg: SdkSettings = {
+          id: this.cfg.id,
+          url: this.cfg.url,
+          bufferDelayMs: this.cfg.bufferDelayMs,
+          avDelayMs: this.cfg.avDelayMs,
+          retryConnectionTimeout: this.cfg.retryConnectionTimeout,
+          onPublishStart: () => {
+            this.runtime.updates.clearAlert('connection-error');
+            this.runtime.updates.raiseEvent({ type: "rtmp-server-connected-and-publishing" })
+          },
+          onConnectionFailure: (reason: RtmpConnectionFailureReason) => {
+            this.runtime.updates.setAlert('connection-error', { level: 'error', message: "Failed to connect" });
+            switch (reason) {
+              case RtmpConnectionFailureReason.RtmpConnectionFailedRetry:
+                this.runtime.updates.raiseEvent({ type: "rtmp-server-connection-failed-retry" })
+                break;
+              default:
+                assertUnreachable(reason)
+            }
+          }
+        }
+        this.rtmp = await this.norsk.output.rtmp(rtmpCfg);
+      }
+      this.rtmp.subscribe(subscriptions, (ctx) => {
+        return ctx.streams.length === subscriptions.length;
+      })
+    } else {
+      await this.rtmp?.close();
+      this.rtmp = undefined;
+    }
+  }
+}
+
+
